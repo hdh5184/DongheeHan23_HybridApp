09102b246d8d25498020050e9299576bf2b27299
DongheeHan <hdh5184@gmail.com>
2023-10-14 19:18:16 +0900

    23.10.14 6주차 실습 과제 제출

---
+++ server/app.js
@@ -0,0 +1,15 @@
+const express = require('express'); //express 불러오기
+const app = express();
+const port = 12311;
+
+app.get('/hi', hi);
+app.listen(port, startserver);
+
+function hi(req, res){ //req : 받는 값
+    //res.header("Access-Control-Allow-Origin", "*");
+    res.send('데이터를 불러온 나 dsjhfcdjhcdsofjdshfdsjhfdsoi');
+}
+
+function startserver(){
+    console.log('서버를 시작한 나(http://localhost:${port}/hi)');
+}
